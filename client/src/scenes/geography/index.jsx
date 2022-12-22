569bcba04607175aa7ce71c0aeeb421e77ae00fe
Suraj Shrestha <surajshrestha9@gmail.com>
2022-12-22 12:04:19 +0545

    3. transaction

---
+++ client/src/scenes/geography/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box,useTheme } from '@mui/material'
+import { useGetGeographyQuery } from 'state/api'
+import Header from 'components/Header'
+import { ResponsiveChoropleth } from '@nivo/geo'
+import { geoData } from 'state/geoData'
+
+
+const Geography = () => {
+    const theme=useTheme()
+    const {data}=useGetGeographyQuery();
+    console.log("Geography data", data)
+  return (
+    <div>Geography</div>
+  )
+}
+
+export default Geography;
